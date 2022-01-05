cd64345db0c1b1b9dbc0b5053c29dd4a9f43e1c4
anilaksu1616 <92857442+anilaksu1616@users.noreply.github.com>
2022-01-05 16:16:20 +0100

    Add files via upload

---
+++ login/Time_scheduler/src/main/src/main/java/main.java
@@ -0,0 +1,11 @@
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ login/Time_scheduler/src/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+public class Main {
+    static final String DB_URL = "jdbc:mysql://localhost/time_scheduler";
+    static final String USER = "much2less";
+    static final String PASS = "1234qwer";
+    static final String QUERY = "SELECT * FROM test";
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY)) {
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                System.out.print("Message: " + rs.getString("message"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
