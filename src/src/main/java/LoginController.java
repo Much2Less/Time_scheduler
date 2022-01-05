391e575626864eec1efe25ff08a19aabbedddcaa
Kristian Mamic <kristianmamic@googlemail.com>
2022-01-05 23:53:42 +0100

    Login-, Register GUI and scene change completed

--- src/src/main/java/LoginController.java
+++ src/src/main/java/LoginController.java
@@ -1,10 +1,17 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class StartScreen extends Application {
+import java.io.IOException;
+
+public class LoginController extends Application {
+
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -24,4 +31,11 @@ public class StartScreen extends Application {
 		}
 	}
 
+	public void switchToRegister(javafx.event.ActionEvent actionEvent) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+		stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 }
--- src/src/main/java/RegisterController.java
+++ src/src/main/java/RegisterController.java
@@ -7,25 +7,16 @@ import javafx.stage.Stage;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
-public class SceneController {
+public class RegisterController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToLogin(ActionEvent actionEvent) throws IOException {
+    public void switchToLogin(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
-
-    public void switchToRegister(javafx.event.ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
-        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
 }
