b2f7a1ec03a074ad589d953eb91cd4b1dce12e67
Kristian Mamic <kristianmamic@googlemail.com>
2022-01-07 00:54:00 +0100

    You can now register a user to the database

--- src/src/main/java/RegisterController.java
+++ src/src/main/java/RegisterController.java
@@ -8,11 +8,15 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterController {
     static final String DB_URL = "jdbc:mysql://localhost/time_scheduler";
-    static final String USER = "root";
-    static final String PASS = "Prabin2468";
+    static final String USER = "much2less";
+    static final String PASS = "1234qwer";
+    static final String QUERY = "INSERT INTO login (username,email,password,admin) VALUES (?, ?, ?, 0)";
 
     @FXML
     private TextField usernameRegister;
@@ -41,18 +45,39 @@ public class RegisterController {
         stage.show();
     }
 
-    public void registerUser() throws IOException {
+    public void registerUser(javafx.event.ActionEvent actionEvent) throws IOException, SQLException {
         username = usernameRegister.getText();
         email = emailRegister.getText();
         password = passwordRegister.getText();
 
         /*
         if(checkRegisterData(username, email, password)) {
+            errorBoxRegister.setText("");
             errorBoxRegister.setText(error.toString());
         }
          */
 
-        //TODO implement code that should register the user in the new database
+        //TODO Password should be hashed
+
+
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement stmt = conn.prepareStatement(QUERY);
+        ) {
+            stmt.setString(1, username);
+            stmt.setString(2, email);
+            stmt.setString(3, password);
+
+            stmt.executeUpdate();
+
+            System.out.println("Success!");
+            switchToLogin(actionEvent);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
         //TODO implement an exception if the username or email is already existing in the database
 
     }
@@ -66,8 +91,31 @@ public class RegisterController {
         Password has no numbers
      */
 /*
-    private boolean checkRegisterData(String username, String email, String password) {
-    }
-*/
 
+    private boolean checkRegisterData(String username, String email, String password) {
+        boolean return_val = false;
+
+        if (username.length() < 4) {
+            return_val = true;
+            error.append("Username is too short!\n");
+        }
+        if (username.length() >= 100) {
+            return_val = true;
+            error.append("Username is too long!\n");
+        }
+
+        Pattern p = Pattern.compile("[^a-z0-9]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(username);
+        boolean res = m.find();
+        if (res) {
+            return_val = true;
+            error.append("Username has special characters!\n");
+        }
+
+
+
+        return return_val;
+    }
+
+*/
 }
