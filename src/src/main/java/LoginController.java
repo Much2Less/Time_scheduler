ddf12c5c891027b8b4195f247f2b8cf7b7c4252f
Kristian Mamic <kristianmamic@googlemail.com>
2022-01-07 01:49:09 +0100

    Registered Users can login to their account now

--- src/src/main/java/LoginController.java
+++ src/src/main/java/LoginController.java
@@ -1,15 +1,31 @@
  import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+ import javafx.fxml.FXML;
+ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+ import javafx.scene.control.TextField;
+ import javafx.stage.Stage;
 import java.io.IOException;
+ import java.security.NoSuchAlgorithmException;
+ import java.sql.*;
 
-import static javafx.fxml.FXMLLoader.load;
+ import static javafx.fxml.FXMLLoader.load;
 
 public class
 LoginController extends Application {
+	static final String DB_URL = "jdbc:mysql://localhost/time_scheduler";
+	static final String USER = "much2less";
+	static final String PASS = "1234qwer";
+	static final String QUERY = "SELECT username password FROM login WHERE username = ? AND password = ?";
+
+	@FXML
+	private TextField usernameLogin;
+	@FXML
+	private TextField passwordLogin;
+
+	private String username;
+	private String password;
 
 	private Stage stage;
 	private Scene scene;
@@ -40,11 +56,35 @@ LoginController extends Application {
 		stage.setScene(scene);
 		stage.show();
 	}
-	public void switchToCalender(javafx.event.ActionEvent actionEvent) throws IOException {
-		Parent root = load(getClass().getResource("calender.fxml"));
-		stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+	public void switchToCalender(javafx.event.ActionEvent actionEvent) throws IOException, NoSuchAlgorithmException {
+		username = usernameLogin.getText();
+		password = passwordLogin.getText();
+
+		HashedPassword hashedPassword = new HashedPassword(password);
+		String hash = hashedPassword.getHashString();
+
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			 PreparedStatement stmt = conn.prepareStatement(QUERY);
+		) {
+			stmt.setString(1, username);
+			stmt.setString(2, hash);
+
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) {
+				Parent root = load(getClass().getResource("calender.fxml"));
+				stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
+			else {
+				System.out.println("Not found!");
+			}
+			//TODO Display an Error Message if Username was not found or password is wrong
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
