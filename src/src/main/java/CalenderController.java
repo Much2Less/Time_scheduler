e70aa1e7dda14f2065ae49be3bfe92c866e9698c
Prabin <prabingaire22@gmail.com>
2022-01-24 12:03:11 +0100

    add appointment extended with the reminder and Priority options. all working

--- src/src/main/java/CalenderController.java
+++ src/src/main/java/CalenderController.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,6 +8,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,14 +16,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 import static javafx.fxml.FXMLLoader.load;
 
-public class CalenderController {
+public class CalenderController implements Initializable {
     static final String DB_URL = "jdbc:mysql://localhost/time_scheduler";
     static final String USER = "root";
     static final String PASS = "Prabin2468";
-    static final String QUERY = "INSERT INTO appointment (name,date,start,startminutes,end,endminutes,location,participants,reminder) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)";
+    static final String QUERY = "INSERT INTO appointment (name,date,start,startminutes,end,endminutes,location,participants,priority,reminder) VALUES (?,?,?, ?, ?, ?, ?, ?, ?, ?)";
 
     @FXML
     private TextField eventName;
@@ -42,13 +45,9 @@ public class CalenderController {
     @FXML
     private TextField eventort;
     @FXML
-    private CheckBox High;
+    private ChoiceBox<String> priority;
     @FXML
-    private CheckBox Medium;
-    @FXML
-    private CheckBox Low;
-    @FXML
-    private TextField threehours;
+    private ChoiceBox<String> reminderbox;
 
 
     private String eventNameAppointment;
@@ -64,6 +63,12 @@ public class CalenderController {
     private CheckBox MediumAppointment;
     private CheckBox LowAppointment;
     private String threehoursAppointment;
+    private String priorityAppointment;
+    private ChoiceBox reminderboxAppointment;
+    private String[] option = {"High","Medium","Low"};
+    private String[] option2 = {"1 Week","3 Days","1 Hour","10 Minutes"};
+
+
 /*
     public void getDate(ActionEvent event) {
         LocalDate date = dateAppointment.getValue();
@@ -73,8 +78,11 @@ public class CalenderController {
             private Stage stage;
             private Scene scene;
 
-            public void switchToRegister(javafx.event.ActionEvent actionEvent) throws IOException {
-            Parent root = load(Objects.requireNonNull(getClass().getResource("optionMenu.fxml")));
+    public CalenderController() {
+    }
+
+    public void switchToRegister(javafx.event.ActionEvent actionEvent) throws IOException {
+            Parent root = load(Objects.requireNonNull(getClass().getResource("optionmenu.fxml")));
             stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
             stage.setTitle("Welcome");
             scene = new Scene(root);
@@ -82,7 +90,7 @@ public class CalenderController {
             stage.show();
             }
 
-    public void creatAppointment(ActionEvent actionEvent) throws IOException, SQLException {
+    public void creatAppointment(ActionEvent actionEvent) throws IOException, SQLException, NoSuchAlgorithmException {
         eventNameAppointment = eventName.getText();
          LocalDate dateAppointment = date.getValue();
         hourAppointment = hour.getText();
@@ -91,11 +99,13 @@ public class CalenderController {
         durationMinutesAppointment = durationMinutes.getText();
         locationAppointment = eventort.getText();
         listParticipantsAppointment = listParticipants.getText();
-        reminderAppointment= reminder.getText();
+        priorityAppointment =priority.getValue();
+        reminderAppointment= reminderbox.getValue();
+
 
 
         try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-             PreparedStatement stmt = conn.prepareStatement(QUERY)
+             PreparedStatement stmt = conn.prepareStatement(QUERY);
         ) {
             stmt.setString(1, eventNameAppointment);
             stmt.setString(2, String.valueOf(dateAppointment));
@@ -105,7 +115,8 @@ public class CalenderController {
             stmt.setString(6, durationMinutesAppointment);
             stmt.setString(7, locationAppointment);
             stmt.setString(8, listParticipantsAppointment);
-            stmt.setString(9, reminderAppointment);
+            stmt.setString(9, priorityAppointment);
+            stmt.setString(10, reminderAppointment);
 
 
 
@@ -115,4 +126,19 @@ public class CalenderController {
         }
 
     }
+    public void canclebutton(javafx.event.ActionEvent actionEvent) throws IOException {
+        Parent root = load(Objects.requireNonNull(getClass().getResource("optionmenu.fxml")));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle("Welcome");
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        priority.getItems().addAll(option);
+        reminderbox.getItems().addAll(option2);
+    }
+
 }
