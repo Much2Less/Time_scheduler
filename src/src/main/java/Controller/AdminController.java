a43d03c2947a6841fe99019299f90751b1506453
Kristian Mamic <kristianmamic@googlemail.com>
2022-02-07 19:55:59 +0100

    Completed the Edit Screen for Adminscreen

--- src/src/main/java/Controller/AdminController.java
+++ src/src/main/java/Controller/AdminController.java
@@ -8,16 +8,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import static javafx.fxml.FXMLLoader.load;
@@ -28,6 +29,7 @@ public class AdminController implements Initializable {
     static final String PASS = "1234qwer";
     static final String SELECT_FROM_LOGIN = "SELECT * FROM login";
     static final String DELETE_USER = "DELETE FROM login WHERE id = ?";
+    static final String EDIT_USER = "UPDATE login SET username = ?, email = ?, admin = ? WHERE id = ?";
 
     private final ArrayList<User> userArrayList = new ArrayList<>();
 
@@ -108,11 +110,6 @@ public class AdminController implements Initializable {
 
     }
 
-    public void editUser(int index) {
-
-    }
-
-
     public void confirmationEvent(ActionEvent actionEvent) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete this User?");
         alert.show();
@@ -133,12 +130,87 @@ public class AdminController implements Initializable {
     private Stage stage;
     private Scene scene;
 
-    public void backtoLogin(javafx.event.ActionEvent actionEvent) throws IOException {
+    public void switchToLogin(javafx.event.ActionEvent actionEvent) throws IOException {
         Parent root = load(Objects.requireNonNull(getClass().getClassLoader().getResource("login.fxml")));
         stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         stage.setTitle("Register");
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void editUser() {
+        if(userListView.getSelectionModel().isEmpty()) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("ERROR");
+            errorAlert.setContentText("You have to select a user!");
+            errorAlert.showAndWait();
+        }
+
+        else {
+            Dialog<AdminController> editDialog = new Dialog<>();
+            //Get Username of edited User and set as a title
+            editDialog.setTitle(selectedUser.getUsername());
+
+            editDialog.setHeaderText("Edit the data of the User:" + selectedUser.getUsername());
+
+            //Setting the Text fields to edit the user
+            Label usernameLabel = new Label("Username: ");
+            Label emailLabel = new Label("E-Mail: ");
+            Label adminLabel = new Label("Admin Status: ");
+            TextField usernameField = new TextField();
+            TextField emailField = new TextField();
+            TextField adminField = new TextField();
+
+            //Adds a grid to add the text fields and labels to the content field of the dialog
+            GridPane gridPane = new GridPane();
+            gridPane.add(usernameLabel, 1, 1);
+            gridPane.add(usernameField, 2, 1);
+            gridPane.add(emailLabel, 1, 2);
+            gridPane.add(emailField, 2, 2);
+            gridPane.add(adminLabel, 1, 3);
+            gridPane.add(adminField, 2, 3);
+            editDialog.getDialogPane().setContent(gridPane);
+
+            //Declares and adds the ButtonType for a callback, when the ok button is clicked
+            ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
+            editDialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+            editDialog.setResultConverter(param -> {
+                if (param == buttonTypeOk) {
+                    if (
+                            //Checks if Fields are not empty
+                            !(
+                            usernameField.getText() == null ||
+                            emailField.getText() == null ||
+                            adminField.getText() == null
+                            )
+                    ) {
+                        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                             PreparedStatement stmt = conn.prepareStatement(EDIT_USER)
+                        ) {
+                            stmt.setString(1, usernameField.getText());
+                            stmt.setString(2, emailField.getText());
+                            stmt.setInt(3, Integer.parseInt(adminField.getText()));
+                            stmt.setInt(4, selectedUser.getId());
+                            stmt.executeUpdate();
+
+                            //Updates the user in the current Arraylist with the text field content
+                            userArrayList.get(selectedUserIndex).setUsername(usernameField.getText());
+                            userArrayList.get(selectedUserIndex).setEmail(emailField.getText());
+                            userArrayList.get(selectedUserIndex).setAdmin(Integer.parseInt(adminField.getText()));
+                        } catch (SQLException throwable) {
+                            throwable.printStackTrace();
+                        }
+                    }
+                }
+                return null;
+            });
+
+            Optional<AdminController> result = editDialog.showAndWait();
+            result.ifPresent(System.out::println);
+
+
+        }
+
     }
 }
--- src/src/main/java/Controller/LoginController.java
+++ src/src/main/java/Controller/LoginController.java
@@ -25,8 +25,8 @@ import static javafx.fxml.FXMLLoader.load;
 public class
 LoginController extends Application {
 	static final String DB_URL = "jdbc:mysql://localhost/time_scheduler";
-	static final String USER = "root";
-	static final String PASS = "Passwort123";
+	static final String USER = "much2less";
+	static final String PASS = "1234qwer";
 	static final String QUERY = "SELECT * FROM login WHERE username = ? AND password = ?";
 
 	@FXML
