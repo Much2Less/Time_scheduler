aea3b83fe9e04aafae1044f8dc7002fbbd68fe77
jrog187 <rogeryampangjr@gmail.com>
2022-02-14 23:57:31 +0100

    PDF structure improved

--- src/src/main/java/Object/DBData.java
+++ src/src/main/java/Object/DBData.java
@@ -4,7 +4,7 @@ package Object;
  * A class to store the login credentials for the mysql database
  */
 public class DBData {
-    final static String DBUser = "much2less";
+    final static String DBUser = "root";
     final static String DBPassword = "1234qwer";
 
     public DBData() {}
